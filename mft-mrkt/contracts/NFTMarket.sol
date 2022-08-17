78102efe9079eb0d32feb96f1dc127bbee81434b
zarif007 <zarifhuq786@gmail.com>
2022-08-17 17:26:00 +0600

    Added testing for 1st contract

--- mft-mrkt/contracts/NFTMarket.sol
+++ mft-mrkt/contracts/NFTMarket.sol
@@ -2,7 +2,19 @@
 pragma solidity ^0.8.16;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTMarket {
+contract NFTMarket is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-}  
+    constructor() ERC721("Gaming NFTs", "GNFT"){}
+
+    function createNFT(string calldata tokenURI) public returns(uint256) {
+        _tokenIds.increment();
+        uint256 currentID = _tokenIds.current();
+        _safeMint(msg.sender, currentID);
+        _setTokenURI(currentID, tokenURI);
+        return currentID;
+    }
+ }  
--- mft-mrkt/hardhat.config.ts
+++ mft-mrkt/hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-ethers");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
